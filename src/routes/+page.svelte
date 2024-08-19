7787a955018b67d2a12d1cef7c10422d40ecc8c0
Wally Flowers <wallyflowers@proton.me>
2024-08-19 13:37:52 -0500

    Add profile picture

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import Post from '../posts/post.md';
+  import profile-pic from '$lib/assets/profile-pic.png';
 </script>
 
 <h1>Wally's Wall</h1>
+
+<img alt="Wally's Profile Picture" src={profile-pic}
 
 <Post />
 

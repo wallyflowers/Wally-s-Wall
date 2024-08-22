c9bba06193522a7efdf90ee241222b404550706d
Wally Flowers <wallyflowers@proton.me>
2024-08-22 09:21:03 -0500

    Configure static adapter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,4 @@
 	import '../app.css';
 </script>
 
-<!-- <svelte:head>
-	<script
-		data-name="BMC-Widget"
-		data-cfasync="false"
-		src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-		data-id="wallyflowers"
-		data-description="Support me on Buy me a coffee!"
-		data-message=""
-		data-color="#FF813F"
-		data-position="Right"
-		data-x_margin="18"
-		data-y_margin="18"
-	></script>
-</svelte:head> -->
-
 <slot />
